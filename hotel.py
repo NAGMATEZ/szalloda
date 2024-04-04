33038e2e00b8a5fbbaaf1bbbfb3ad056c9f080ab
NAGMATEZ <nagymate32@gmail.com>
2024-04-04 11:53:11 +0200

    Whitespace-ekkel tisztábbá tettem a kódot, és a pycharm hatására a menüt példányosítva használom, hogy a függvényei tudják a self-et használni

--- hotel.py
+++ hotel.py
@@ -1,10 +1,13 @@
 from abc import abstractmethod
+
+
 class Szoba:
 
-    foglalt=False
-    def __init__(self, ar:int, szobaszam:int):
-        self.ar=ar
-        self.szobaszam=szobaszam
+    foglalt = False
+
+    def __init__(self, ar: int, szobaszam: int):
+        self.ar = ar
+        self.szobaszam = szobaszam
 
     
 class EgyAgyasSzoba(Szoba):
@@ -12,90 +15,94 @@ class EgyAgyasSzoba(Szoba):
     def __init__(self, ar: int, szobaszam: int):
         super().__init__(ar, szobaszam)
 
+
 class KetAgyasSzoba(Szoba):
 
     def __init__(self, ar: int, szobaszam: int):
         super().__init__(ar, szobaszam)
 
+
 class Szalloda:
-    szobak=[]
+    szobak = []
+
     def __init__(self, nev):
-        self.nev=nev
+        self.nev = nev
 
     def szobahozzaadas(self, szoba):
         self.szobak.append(szoba)
 
 
 class Foglalas:
-    def __init__(self, ev:int, honap:int, nap:int, agyszam:int, szobaszam:int, ugyfelnev:str):
-        self.ev=ev
-        self.honap=honap
-        self.nap=nap
-        self.agyszam=agyszam
-        self.szobaszam=szobaszam
-        self.ugyfelnev=ugyfelnev
-
+    def __init__(self, ev: int, honap: int, nap: int, agyszam: int, szobaszam: int, ugyfelnev: str):
+        self.ev = ev
+        self.honap = honap
+        self.nap = nap
+        self.agyszam = agyszam
+        self.szobaszam = szobaszam
+        self.ugyfelnev = ugyfelnev
 
 
 def foglal():           
-    agyszam=int(input("Hány ágyas szobát szeretne? (1 vagy 2): "))
+    agyszam = int(input("Hány ágyas szobát szeretne? (1 vagy 2): "))
     print("Kérem a dátumokat ne kezdje 0-val ; pl:01 -> 1")
     while True:
-        ev=int(input("Kérem adja meg a foglalás évét: "))
-        if ev<=2024:
+        ev = int(input("Kérem adja meg a foglalás évét: "))
+        if ev <= 2024:
             break
     while True:
-        honap=int(input("Kérem adja meg a foglalás hónapját: "))
-        if 1<=honap<=12:
+        honap = int(input("Kérem adja meg a foglalás hónapját: "))
+        if 1 <= honap <= 12:
             break
     while True:
-        nap=int(input("Kérem adja meg a foglalás napját: "))
-        if honap==1 or honap==3 or honap==5 or honap==7 or honap==8 or honap==10 or honap==12: 
-            if 1<=nap<=31:
+        nap = int(input("Kérem adja meg a foglalás napját: "))
+        if honap == 1 or honap == 3 or honap == 5 or honap == 7 or honap == 8 or honap == 10 or honap == 12:
+            if 1 <= nap <= 31:
                 break
-        elif honap==4 or honap==6 or honap==9 or honap==11:
-            if 1<=nap<=30:
+        elif honap == 4 or honap == 6 or honap == 9 or honap == 11:
+            if 1 <= nap <= 30:
                 break
         else:
-            if ev-2024%4==0:
-                if 1<=nap<=29:
+            if ev-2024 % 4 == 0:
+                if 1 <= nap <= 29:
                     break
             else:
-                if 1<=nap<=28:
+                if 1 <= nap <= 28:
                     break
     while True:
-        ugyfelnev=input("Milyen névre lesz a foglalás?: ")
-        if ugyfelnev!="":
+        ugyfelnev = input("Milyen névre lesz a foglalás?: ")
+        if ugyfelnev != "":
             break
 
-    if agyszam==1:
+    if agyszam == 1:
         for szoba in Szalloda.szobak:
-            if isinstance(szoba, EgyAgyasSzoba) and szoba.foglalt==False:
-                szobaszam=szoba.szobaszam
-                szoba.foglalt=True
+            if isinstance(szoba, EgyAgyasSzoba) and szoba.foglalt == False:
+                szobaszam = szoba.szobaszam
+                szoba.foglalt = True
                 foglalasok.append(Foglalas(ev, honap, nap, 1, szobaszam, ugyfelnev))
                 return szoba.ar
-    elif agyszam==2:
+    elif agyszam == 2:
         for szoba in Szalloda.szobak:
-            if isinstance(szoba, EgyAgyasSzoba) and szoba.foglalt==False:
-                szobaszam=szoba.szobaszam
-                szoba.foglalt=True
+            if isinstance(szoba, EgyAgyasSzoba) and szoba.foglalt == False:
+                szobaszam = szoba.szobaszam
+                szoba.foglalt = True
                 foglalasok.append(Foglalas(ev, honap, nap, 2, szobaszam, ugyfelnev))
                 return szoba.ar
     else:
         print("A szállodában nincs szabad szoba a kért feltételekkel")
+    print("Köszönjük foglalását!\n")
 
 
 def lemondas():
-    ugyfelnev=input("Milyen névre volt a lemondani kívánt foglalás?: ")
-    ev=int(input("Kérem adja meg a lemondani kívánt foglalás évét: "))
-    honap=int(input("Kérem adja meg a lemondani kívánt foglalás hónapját: "))
-    nap=int(input("Kérem adja meg a lemondani kívánt foglalás napját: "))
-    szobaszam=int(input("Kérem adja meg a lemondani kívánt foglaláshoz kapcsolt szobaszámot: "))
-    agyszam=int(input("Hány ágyas szobára foglalt?: "))
+    ugyfelnev = input("Milyen névre volt a lemondani kívánt foglalás?: ")
+    ev = int(input("Kérem adja meg a lemondani kívánt foglalás évét: "))
+    honap = int(input("Kérem adja meg a lemondani kívánt foglalás hónapját: "))
+    nap = int(input("Kérem adja meg a lemondani kívánt foglalás napját: "))
+    szobaszam = int(input("Kérem adja meg a lemondani kívánt foglaláshoz kapcsolt szobaszámot: "))
+    agyszam = int(input("Hány ágyas szobára foglalt?: "))
 
     for foglalas in foglalasok:
-        if foglalas.ev==ev and foglalas.honap==honap and foglalas.nap==nap and foglalas.agyszam==agyszam and foglalas.szobaszam==szobaszam and foglalas.ugyfelnev==ugyfelnev:
+        if (foglalas.ev == ev and foglalas.honap == honap and foglalas.nap == nap and foglalas.agyszam == agyszam and
+                foglalas.szobaszam == szobaszam and foglalas.ugyfelnev == ugyfelnev):
              del(foglalas)
              print("A lemondás sikeres volt! Reméljük, legközelebb találkozunk")
              break
@@ -105,26 +112,27 @@ def lemondas():
 def foglalaslistazas(foglalasok):
     print("Az összes foglalás: \n")
     for foglalas in foglalasok:
-        print("Dátum: " + str(foglalas.ev) + ". " + str(foglalas.honap) + ". " + str(foglalas.nap) +  "; Név: " + str(foglalas.ugyfelnev) + " ; Szobaszám: " + str(foglalas.szobaszam) +" ; Ágyak száma a szobában: " + str(foglalas.agyszam) + "\n")
+        print("Dátum: " + str(foglalas.ev) + ". " + str(foglalas.honap) + ". " + str(foglalas.nap) + "; Név: " + str(foglalas.ugyfelnev) + " ; Szobaszám: " + str(foglalas.szobaszam) + " ; Ágyak száma a szobában: " + str(foglalas.agyszam) + "\n")
+
 
 class Menuinterfesz:
     @abstractmethod
-    def opciok():
+    def opciok(self):
          #Megjeleníti a menü opcióit
          pass
     @abstractmethod
-    def getAnswer():
+    def getAnswer(self):
         #Megszerzi a felhasználótól a választ, és aszerint megy tovább
         pass
 class Menu:
 
-    def opciok():
+    def opciok(self):
         print("Üdvözöljük a " + TestSzallo.nev + " szállodában! \n Készüléke nyomógombjai segítségével kérem válasszon az alábbi menüpontok közül: ")
         print("Foglalás kezdeményezése      - 1-es gomb")
         print("Meglévő foglalás lemondása   - 2-es gomb")
         print("Az összes foglalás listázása - 3-as gomb")
 
-    def getAnswer():
+    def getAnswer(self):
         while True:
             try:
                 valasz=int(input("\n Kérem írja be választását! (1,2,3): "))
@@ -135,34 +143,32 @@ class Menu:
                 print("A bevitel formátuma nem megfelelő!! Kérem számmal válasszon a menüből")
 
 
-
-foglalasok=[
-Foglalas(2024,12,24,2,21,"Nagy Máté"),
-Foglalas(2026,1,20,1,11,"Kelemen Ármin"),
-Foglalas(2024,6,6,1,12,"Szilágyi Judit"),
-Foglalas(2028,2,26,1,11,"Kocsis Szilárd"),
-Foglalas(2030,7,15,2,21,"Nagy Johanna")
+foglalasok = [
+Foglalas(2024, 12, 24, 2, 21, "Nagy Máté"),
+Foglalas(2026, 1, 20, 1, 11, "Kelemen Ármin"),
+Foglalas(2024, 6, 6, 1, 12, "Szilágyi Judit"),
+Foglalas(2028, 2, 26, 1, 11, "Kocsis Szilárd"),
+Foglalas(2030, 7, 15, 2, 21, "Nagy Johanna")
 ]
 
+M = Menu()
+TestSzallo = Szalloda(nev="Tesztellek")
 
-TestSzallo=Szalloda(nev="Tesztellek")
+TestSzallo.szobahozzaadas(EgyAgyasSzoba(12000, 11))
 
-TestSzallo.szobahozzaadas(EgyAgyasSzoba(12000,11))
+TestSzallo.szobahozzaadas(EgyAgyasSzoba(15000, 12))
 
-TestSzallo.szobahozzaadas(EgyAgyasSzoba(15000,12))
-
-TestSzallo.szobahozzaadas(KetAgyasSzoba(20000,21))
+TestSzallo.szobahozzaadas(KetAgyasSzoba(20000, 21))
 while True:
-    Menu.opciok()
+    Menu.opciok(M)
 
-    val=Menu.getAnswer()
+    val = Menu.getAnswer(M)
 
-    if val==1:
+    if val == 1:
         foglal()
-    elif val==2:
+    elif val == 2:
         lemondas()
-    elif val==3:
+    elif val == 3:
         foglalaslistazas(foglalasok)
     else: 
         print("Ismeretlen hiba")
-
